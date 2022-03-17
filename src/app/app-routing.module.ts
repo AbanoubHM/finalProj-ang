076e42b64cec72506e1d06a822b961da423f86b7
Ahmed <ashalaby123123@gmail.com>
2022-03-18 00:33:14 +0200

    Carts commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { RegisterComponent } from './components/register/register.component';
 import { HomeComponent } from './components/home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CartsComponent } from './components/carts/carts.component';
 
 const routes: Routes = [
   {path:'',component:HomeComponent},
   {path:'register',component:RegisterComponent}
+  ,{path:'cart',component:CartsComponent}
 ];
 
 @NgModule({
