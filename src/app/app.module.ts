cb357a2cd0d984c698b6d7e6372e4c00eef4848c
Ahmed <ashalaby123123@gmail.com>
2022-03-17 22:35:40 +0200

    Adding Carts Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HomeComponent } from './components/home/home.component';
+import { CartsComponent } from './components/carts/carts.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     FooterComponent,
-    HomeComponent
+    HomeComponent,
+    CartsComponent
   ],
   imports: [
     BrowserModule,
