54955b7276a092a1dc6a3366afe75f5a0376e8a9
hussain mabeed <hussainmabeed@gmail.com>
2022-03-24 14:33:02 +0200

    edit in orders

--- src/app/components/orders/orders.component.ts
+++ src/app/components/orders/orders.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit , Input } from '@angular/core';
 
 @Component({
   selector: 'app-orders',
@@ -8,7 +8,6 @@ import { Component, OnInit } from '@angular/core';
 export class OrdersComponent implements OnInit {
 
   constructor() { }
-
   ngOnInit(): void {
   }
 
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,13 +1,13 @@
 
 // import { from } from "rxjs";
-import {domain, clientId} from '../../auth_config.json'
+// import {domain, clientId} from '../auth_config.json'
 export const environment = {
   production: false,
-  auth:{
-    domain,
-    clientId,
-    redirectUri:window.location.origin
-  }
+  // auth:{
+  //   domain,
+  //   // clientId,
+  //   redirectUri:window.location.origin
+  // }
 };
 
 
