058e1535b889a012dfc10c3441730f8dc5c85ad7
hussain mabeed <hussainmabeed@gmail.com>
2022-03-24 16:29:38 +0200

    orders

--- src/app/components/orders/orders.component.ts
+++ src/app/components/orders/orders.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit , Input } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-orders',
   templateUrl: './orders.component.html',
@@ -7,8 +7,12 @@ import { Component, OnInit , Input } from '@angular/core';
 })
 export class OrdersComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router :Router) { }
   ngOnInit(): void {
+    
+
+
+
   }
 
 }
