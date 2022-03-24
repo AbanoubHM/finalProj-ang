a7bdc4acc0242a783984a5805f61327cd18335e3
hussain mabeed <hussainmabeed@gmail.com>
2022-03-24 20:29:40 +0200

    gatogry

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { GategoryService } from 'src/app/Service/gategory.service';
+
 
 @Component({
   selector: 'app-dashboard',
@@ -7,10 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  constructor(private gategory : GategoryService) { }
   showFiller = false;
+  public gat : any = [];
 
   ngOnInit(): void {
+    this.gategory.getAllPosts().subscribe(res=>{
+      this.gat = res;
+    })
+
   }
 
 }
