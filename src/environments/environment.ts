7ec85d36819f41876e5e1f004b38668548090744
Abanoub Harby <abanoubharby33@gmail.com>
2022-04-05 16:46:42 +0200

    Authentication

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,28 +1,23 @@
 // import {audience,appUri} from '../auth_config.json'
 import * as config from '../auth_config.json';
-const { domain,clientId,audience,appUri,apiUri }=config as {
-  domain:string
-  clientId:string
-  audience:string,
-  appUri:string,
-  apiUri:string
-}
+const { domain, clientId, audience, apiUri, errorPath } = config as {
+  domain: string;
+  clientId: string;
+  audience: string;
+  apiUri: string;
+  errorPath: string;
+};
 export const environment = {
   production: false,
-  jsonServer : "http://localhost:3000",
-  auth:{
-    domain,
-    clientId,
-    useRefreshTokens: true,
-    redirectUri:window.location.origin,
-    audience
-  },
-  dev:{
-    appUri
+  jsonServer: 'http://localhost:3000',
+  auth: {
+    domain: 'dev-vxrkxu-x.us.auth0.com',
+    clientId: 'i8fVcQjbbcHMJzi4uelRAgS2k2UFeC9m',
+    audience: 'https://localhost:7263/',
+    redirectUri: window.location.origin,
+    errorPath: '/error',
   },
   httpInterceptor: {
     allowedList: [`${apiUri}/*`],
   },
-
 };
-
