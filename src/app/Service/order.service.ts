0268bd3493c58e03d34d47f89de64aebd5642c74
Azza893 <azzae893@gmail.com>
2022-04-05 14:15:54 +0200

    order service

--- src/app/Service/order.service.ts
+++ src/app/Service/order.service.ts
@@ -1,9 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OrderService {
+  private url = "https://handmadeapi.azurewebsites.net/api/Orders";
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  GetUserOrders(id: any) {
+    this.http.get(`${this.url}/${id}`)
+  }
+  AddOrder(item: any) {
+    this.http.post(`${this.url}`, item);
+  }
+  GetOrderDetails(orderid: any) {
+    this.http.get(`${this.url}/details/${orderid}`)
+  }
 }
